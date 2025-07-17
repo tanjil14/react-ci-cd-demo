f1d164d7e1385dec8eb0204b5c46bb478cec41f7
tanjil14 <tanjil14@cse.pstu.ac.bd>
2025-07-17 15:30:33 +0600

    configure github page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>This is ci/cd demo</h2>
+        <p>Deploy in github pages</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
